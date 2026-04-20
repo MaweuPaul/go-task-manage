6ed7b96f3c99405555ff6885ad935d5c400500bc
MaweuPaul <maweupaul85@gmail.com>
2026-04-20 11:48:40 +0300

    Refactor GenerateJWT and RefreshJwt functions to improve user ID validation and code structure

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -9,12 +10,36 @@ import (
 
 // function to generate a JWT token for a user
 func GenerateJWT(userId string) (string, error) {
+
+	if userId == "" {
+		return "", fmt.Errorf("User id is required")
+	}
 	// creat a claim
 	claims := jwt.MapClaims{
 		"userId": userId,
 		"exp":    time.Now().Add(time.Hour * 72).Unix(), // token expires in 72 hours
+		"type":   "access",
 	}
 	// create a token with the claims and sign it with a secret key
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret := os.Getenv("jwt_secret")
+	return token.SignedString([]byte(secret))
+
+}
+
+// refresh token
+
+func RefreshJwt(userId string) (string, error) {
+
+	if userId == "" {
+		return "", fmt.Errorf("User id is required")
+	}
+
+	claims := jwt.MapClaims{
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour * 24 * 30).Unix(),
+		"type":   "refresh",
+	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	secret := os.Getenv("jwt_secret")
 	return token.SignedString([]byte(secret))
