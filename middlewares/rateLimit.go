c27dfc74f5d5a6a1a9d7b0fd7385054c5567ce70
MaweuPaul <maweupaul85@gmail.com>
2026-04-25 20:57:46 +0300

    Add rate limit exceeded handler to RateLimitMiddleware

--- middlewares/rateLimit.go
+++ middlewares/rateLimit.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ulule/limiter/v3"
 	mgin "github.com/ulule/limiter/v3/drivers/middleware/gin"
@@ -14,5 +16,11 @@ func RateLimitMiddleware(rateFormat string) gin.HandlerFunc {
 	}
 	store := memory.NewStore()
 	instance := limiter.New(store, rate)
-	return mgin.NewMiddleware(instance)
+
+	return mgin.NewMiddleware(instance, mgin.WithLimitReachedHandler(func(c *gin.Context) {
+		c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+			"error":   "too many requests",
+			"message": "you have exceeded the rate limit, please try again later",
+		})
+	}))
 }
